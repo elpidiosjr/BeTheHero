311be1c8bce9c2ea24cf422f71c22fab1983818f
Elpídio Soares Junior <101202647+elpidiosjr@users.noreply.github.com>
2022-07-12 14:13:32 -0300

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,16 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+// get: é para buscar informaçao do backend
+
+// post: criar uma informaçao no backend
+
+// put: alterar uma informaçao no backend
+
+// delete: deletar informaçao no backend
+
+
+app.get('/users', (request, response) => {
     return response.json({
         evento: 'Semana OmniStack 11.0',
         aluno: 'Elpidio Soares'
