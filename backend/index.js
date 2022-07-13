2b8de76caff0e4d2e4b33c14a24d8adbc7e0ce78
Elpídio Soares Junior <101202647+elpidiosjr@users.noreply.github.com>
2022-07-13 17:04:52 -0300

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,37 @@ const express = require('express');
 
 const app = express();
 
-// get: é para buscar informaçao do backend
+app.use(express.json());
 
-// post: criar uma informaçao no backend
+// GET: é para buscar/listar informaçao do backend
 
-// put: alterar uma informaçao no backend
+// POST: criar uma informaçao no backend
 
-// delete: deletar informaçao no backend
+// PUT: alterar uma informaçao no backend
 
+// DELETE: deletar informaçao no backend
 
-app.get('/users', (request, response) => {
+/*
+    Tipos de parâmetros:
+    Query Params: parâmetros nomeados enviados na rota após *?* (filtros, paginação)
+    Route Params: Parâmetros utilizados para identificar recursos
+    Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+*/
+
+/* SQL : MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server.
+   NoSQL: MongoDB, CouchDB, etc.
+*/
+
+/*
+Driver: SELECT * FROM users
+Query Builder: table('users').select('*').where()
+*/
+
+app.post('/users', (request, response) => {
+    const body = request.body;
+
+    console.log(body);
+
     return response.json({
         evento: 'Semana OmniStack 11.0',
         aluno: 'Elpidio Soares'
