fe7fc7039b74095cd61ebca84499cb83fc612b0e
Elpidio <elpidiosoares@gmail.com>
2022-07-15 17:53:13 -0300

    profile and session

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -6,6 +6,7 @@ module.exports = {
     return response.json(incidents);
   }, 
 
+
     async create(request, response) {
         const { title, description, value } = request.body;
         const ong_id = request.headers.authorization;
@@ -18,5 +19,22 @@ module.exports = {
         });
 
       return response.json({ id }); 
+    },
+
+    async delete(request, response) {
+      const { id } = request.params;
+      const ong_id = request.headers.authorization;
+
+      const incident = await connection('incidents')
+      .where('id', id)
+      .select('ong_id')
+      .first();
+
+      if (incident.ong_id != ong_id){
+        return response.status(401).json({ error: 'Operation not permitted.' });
+      }  
+      await connection('incidents').where('id', id).delete();
+
+      return response.status(204).send();
     }
 };
