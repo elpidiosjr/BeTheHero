5c56383c28a153fb6130552f29733a3821a8dc41
Elpidio <elpidiosoares@gmail.com>
2022-07-19 18:29:32 -0300

    cors

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,11 @@
-const { application } = require('express');
+
 const express = require('express');
+const cors = require('cors')
+const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-// GET: é para buscar/listar informaçao do backend
-
-// POST: criar uma informaçao no backend
-
-// PUT: alterar uma informaçao no backend
-
-// DELETE: deletar informaçao no backend
-
-/*
-    Tipos de parâmetros:
-    Query Params: parâmetros nomeados enviados na rota após *?* (filtros, paginação)
-    Route Params: Parâmetros utilizados para identificar recursos
-    Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
-*/
-
-/* SQL : MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server.
-   NoSQL: MongoDB, CouchDB, etc.
-*/
-
-/*
-Driver: SELECT * FROM users
-Query Builder: table('users').select('*').where()
-*/
-
-app.post('/users', (request, response) => {
-    const body = request.body;
-
-    console.log(body);
-
-    return response.json({
-        evento: 'Semana OmniStack 11.0',
-        aluno: 'Elpidio Soares'
-    });
-});
-
+app.use(routes);
 app.listen(3333);
