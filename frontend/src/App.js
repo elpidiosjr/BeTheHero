25617fd2548ce0f4f83729c997f8b1978a42eada
Elpidio <elpidiosoares@gmail.com>
2022-07-22 18:09:55 -0300

    imutabilidade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Header from "./Header";   
 function App() {
-  let counter = 0;
-
+  const [counter, setCounter] = useState(0);
+  //retorna Array [valor, funcaoDeAtaulizacao]
 
 function increment() {
-  counter +=1;
-  console.log(counter);
+  setCounter(counter + 1);
 }
 
 
   return (
     <div>
-      <Header>Contador: 0</Header>
+      <Header>Contador: {counter}</Header>
       <button onClick={increment}>Incrementar</button>
     </div>
   );
