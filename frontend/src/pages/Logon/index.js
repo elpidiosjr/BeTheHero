91e251fafea20eb03fc18b3c54d0bb19c6bd668b
Elpidio <elpidiosoares@gmail.com>
2022-07-22 18:35:43 -0300

    global e style css

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './style.css';
+
+export default function Logon() {
+    return (
+        <div className="logon-container"></div>,
+        <section className="form"></section>
+    );
+}
