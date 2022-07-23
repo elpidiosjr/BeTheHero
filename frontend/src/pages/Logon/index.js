3370c1614eb0b3a8d6b6632ba239b302f80ed5d0
Elpidio <elpidiosoares@gmail.com>
2022-07-23 10:12:22 -0300

    tela de login-esqueleto

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi'
 
 import './style.css';
 
+import logoImg from '../../assets/logo.svg'
+import heroesImg from '../../assets/heroes.png';
+
 export default function Logon() {
     return (
-        <div className="logon-container"></div>,
-        <section className="form"></section>
+        <div className="logon-container">
+        <section className="form">
+            <img src={logoImg} alt="Be The Hero" />
+
+            <form>
+                <h1>Faça seu logon</h1>
+                <input placehold="Sua ID"  />
+                <button type="submit">Entrar</button>
+
+                <a href="/register"></a>
+                <FiLogIn size={16} color="#E02041" />
+                Não tenho cadastro
+            </form>
+        </section>
+
+        <img src={heroesImg} alt="Heroes" />
+        </div>
     );
 }
