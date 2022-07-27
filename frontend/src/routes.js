c191e0ce43c22e76de84877a0f2a44917600c461
Elpidio <elpidiosoares@gmail.com>
2022-07-27 12:51:42 -0300

    register

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Logon from './pages/Logon';
+import Register from './pages/Register';
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Logon/>} />
+                <Route path="/register" element={<Register/>} />
+
             </Routes>
         </BrowserRouter>
     );
