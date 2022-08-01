0aa50ab515d9b2307a7ae9a44e5395012cb3b53a
Elpidio <elpidiosoares@gmail.com>
2022-08-01 15:53:54 -0300

    create incident

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,14 +4,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
+
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Logon/>} />
                 <Route path="/register" element={<Register/>} />
-
+                
                 <Route path="/profile" element={<Profile/>} />
+                <Route path="/incidents/new" element={<NewIncident/>} />
+
             </Routes>
         </BrowserRouter>
     );
