b198f6952edf8ce9055c17bdb02d9101eb43d099
Elpidio <elpidiosoares@gmail.com>
2022-08-01 09:23:33 -0300

    create body

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+import {FiPower} from 'react-icons/fi'
+
+import logoImg from '../../assets/logo.svg';
 
 export default function Profile () {
-    return <h1>Profile</h1>
+    return (
+        <div className="profile-containe">
+            <header>
+                <img src={logoImg} alt="Be The Hero" />
+                <span>Bem vinda, APAD</span>
+
+                <Link className="button" to="/incidents/new" > Cadastrar novo caso</Link>
+                <button type="button">
+                    <FiPower size={18}/>
+                </button>
+            </header>
+        </div>
+    );
 }
