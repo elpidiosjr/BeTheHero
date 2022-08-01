b7d2c6c65e5b222e3e9b4e240eb3cac48d6a7b16
Elpidio <elpidiosoares@gmail.com>
2022-08-01 14:06:57 -0300

    css 2ª

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom'
-import {FiPower} from 'react-icons/fi'
+import {FiPower, FiTrash2} from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg';
 import './style.css'
@@ -29,6 +29,52 @@ export default function Profile () {
 
                     <strong>VALOR:</strong>
                     <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#A8A8B3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#A8A8B3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#A8A8B3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#A8A8B3" />
+                    </button>
                 </li>
             </ul>
 
