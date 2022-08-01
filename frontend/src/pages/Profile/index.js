051a1def9da1c3ea240fe3a5144bfbdd76020bd5
Elpidio <elpidiosoares@gmail.com>
2022-08-01 10:07:05 -0300

    css 1ª

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -7,7 +7,7 @@ import './style.css'
 
 export default function Profile () {
     return (
-        <div className="profile-containe">
+        <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero" />
                 <span>Bem vinda, APAD</span>
