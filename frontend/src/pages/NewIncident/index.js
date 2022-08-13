4386ce44986d04579287fa62c0e0163fd2459128
Elpidio <elpidiosoares@gmail.com>
2022-08-13 12:02:09 -0300

    registration

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,12 +1,44 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useNavigate } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './style.css';
 
 import logoImg from '../../assets/logo.svg'
 
 export default function NewIncident() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+
+    const navigate = useNavigate();
+
+    const ongId = localStorage.getItem('ongId');
+
+    async function handleNewIncident(e){
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value,
+        };
+
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    Autorization: ongId,
+                }
+            })
+
+            navigate('/profile');
+        }catch (err) {
+            alert('Erro ao cadastrar caso, tente novamente.')
+        }
+    }
+
     return (
         <div className="new-incident-container">
         <div className="content">
@@ -20,11 +52,24 @@ export default function NewIncident() {
                 Voltar para home
                 </Link>
             </section>
-            <form>
-                <input placeholder ="Título do caso" />
-                <textarea placeholder="Descrição" />
-                <input placeholder="Valor em reais" />
+            <form onSubmit={handleNewIncident} >
+                <input 
+                placeholder ="Título do caso" 
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+                />
 
+                <textarea 
+                placeholder="Descrição" 
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+                />
+
+                <input 
+                placeholder="Valor em reais" 
+                value={value}
+                onChange={e => setValue(e.target.value)}
+                />
             
                 <button className="button" type="submit">Cadastrar</button>
 
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom'
+import { Link, useNavigate} from 'react-router-dom'
 import {FiPower, FiTrash2} from 'react-icons/fi'
 
 import api from '../../services/api';
@@ -9,10 +9,12 @@ import './style.css'
 
 export default function Profile () {
     const [incidents, setIncidents] = useState([]);
+    
+    const navigate = useNavigate();
 
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
-        api.get('')
+       
     useEffect(() => {
             api.get('profile', {
                 headers: {
@@ -30,11 +32,21 @@ export default function Profile () {
                     headers: {
                         Authorization: ongId,
                     }
-                })
+                });
+
+        setIncidents(incidents.filter(incident => incidents.id !== id));
+
         }catch (err) {
             alert('Erro ao deletar caso, tente novamente.');
         }
     }
+
+    function handleLogout() {
+        localStorage.clear();
+
+        navigate('/');
+    }
+
 
     return (
         <div className="profile-container">
@@ -43,7 +55,7 @@ export default function Profile () {
                 <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new" > Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout}  type="button">
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
